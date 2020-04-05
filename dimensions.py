bab6ca42d0dc83a4a5d8729135e8590570ddbb3a
Hongwei <pengrubin@foxmail.com>
2020-04-05 13:25:59 +0800

    banned_user

--- dimensions.py
+++ dimensions.py
@@ -3,8 +3,7 @@ print("Original dimensions:")
 for dimension in dimensions:
     print(dimension)
     
-dimensions = 
-(400, 100)
+dimensions = (400, 100)
 print("\nModified dimensions:")
 for dimension in dimensions:
     print(dimension)
