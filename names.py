52eb82dbfbfd4879c50ebb3054929f6a21e53bbe
Hongwei <pengrubin@foxmail.com>
2020-04-12 19:56:28 +0800

    name_chonggou

---
+++ names.py
@@ -0,0 +1,15 @@
+
+import name_function
+
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+    if first == 'q':
+        break
+        
+    last = input("Please give me a last name: ")
+    if last == 'q':
+        break
+        
+    formatted_name = name_function.get_formatted_name(first, last)
+    print("\tNeatly formatted name: " + formatted_name + '.')
